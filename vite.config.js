06be6e88d2daa8e4d61bc3a41146ddaa408ba6bb
chidera <ofojuochidera2004@gmail.com>
2026-01-03 09:40:02 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // make sure React plugin is installed
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    // This is the important part
+    historyApiFallback: true,
+  },
 });
